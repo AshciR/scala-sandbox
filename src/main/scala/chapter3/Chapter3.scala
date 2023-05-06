080abaa1602da1069be1482f19252beecdebba3f
Richard Walker <richard.walker.90@gmail.com>
2023-05-06 16:08:50 -0400

    Initial Commit

---
+++ src/main/scala/chapter3/Chapter3.scala
@@ -0,0 +1,51 @@
+package chapter3
+
+@main
+def main(): Unit = {
+  //  arrayExample()
+  //  listExample()
+  //  tupleExample()
+  setExample()
+}
+
+
+def setExample(): Unit = {
+  val cities = Set("Kingston", "Miami", "Vegas")
+  println(cities)
+  val moreCities = cities + "Raleigh"
+  println(moreCities)
+}
+
+def tupleExample(): Unit = {
+  val pair = ("Richie", 30)
+  println(pair)
+}
+
+def listExample(): Unit = {
+  val listA = List(1, 2, 3, 4)
+
+  val listB = List(5, 6, 7, 8)
+
+  // concats the list
+  println("Concat list")
+  println(listA ::: listB)
+
+  // Cons operator adds element to the start of the list
+  println("Cons list")
+  println(0 :: listA)
+
+
+}
+
+def arrayExample(): Unit = {
+  val greetStrings = new Array[String](3)
+
+  greetStrings(0) = "Hello "
+  greetStrings(1) = "Richie "
+  greetStrings(2) = "World "
+
+
+  for i <- 0 to 2 do
+    print(greetStrings(i))
+}
+
---
+++ src/main/scala/main.scala
@@ -0,0 +1,5 @@
+@main
+def main(args: String*): Unit = {
+  println("Hello " + args(0) + " world!")
+}
+
